2ab7ef7c53eaf628d1573620ffcace2544ec3fee
bianca.lazzaris <bianca.lazzaris@gmail.com>
2021-04-25 23:15:03 -0300

    partials

--- index.js
+++ index.js
@@ -1,43 +1,36 @@
-const { request, response } = require('express');
 const express = require('express');
-//importando o express no projeto
 const app = express()
 
-//instanciando o EJS
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
-//criar minhas rotas 
+
+app.get('/artigos', (req, res) => {
+    res.render('article')
+});
+
+app.get('/novo-artigo', (req, res) => {
+    res.render('new')
+});
+
+
 app.get('/:name?/:faculdade?', (req,res) => {
-    let name = req.params.name;
-    let faculdade = req.params.faculdade;
+    // let name = req.params.name;
+    // let faculdade = req.params.faculdade;
     let msg = true;
-    // let name = 'Maria da Silva';
-    // let faculdade = 'IELUSC';
+    let name = 'Maria da Silva';
+    let faculdade = 'IELUSC';
 
     res.render('index', {
         name, 
         faculdade,
         remoto: true,
-        curso: 'Desenvolvimento Web2',
+        curso: 'Desenvolvimento Web 2',
         exibirNotificacao: msg,
     });
 });
 
-app.get('/alunos', (req,res) => {
-    let alunos = [
-        {id:1, nome: 'Bianca', genero: 'Feminino'},
-        {id:1, nome: 'Misael', genero: 'Masculino'},
-        {id:1, nome: 'Victor', genero: 'Masculino'},
-        {id:1, nome: 'Lucas', genero: 'Masculino'},
-        {id:1, nome: 'Alexandre', genero: 'Masculino'},
-    ];
-    
-    res.render('alunos', {
-        pessoas: alunos
-    });
 
-});
 
 app.listen(5000, (erro) =>  {
     if (erro) {
@@ -46,3 +39,19 @@ app.listen(5000, (erro) =>  {
         console.log('servidor rodando no endereço: http://localhost:5000')
     }
 });
+
+
+// app.get('/alunos', (req,res) => {
+//     let alunos = [
+//         {id:1, nome: 'Bianca', genero: 'Feminino'},
+//         {id:1, nome: 'Misael', genero: 'Masculino'},
+//         {id:1, nome: 'Victor', genero: 'Masculino'},
+//         {id:1, nome: 'Lucas', genero: 'Masculino'},
+//         {id:1, nome: 'Alexandre', genero: 'Masculino'},
+//     ];
+    
+//     res.render('alunos', {
+//         pessoas: alunos
+//     });
+
+// });
